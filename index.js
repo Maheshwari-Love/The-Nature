d7ec57e293f3372feb5a0b1806c70fb7aee72adb
monilprajapati <monillprajapatii@gmail.com>
2023-08-06 00:42:13 +0530

    Added javascript file and add some functionality

---
+++ index.js
@@ -0,0 +1,37 @@
+const card1 = document.getElementById("card1");
+const card2 = document.getElementById("card2");
+const card3 = document.getElementById("card3");
+const card4 = document.getElementById("card4");
+const card5 = document.getElementById("card5");
+const card6 = document.getElementById("card6");
+const nature = document.getElementById("nature");
+
+card1.addEventListener("click", card1Visit);
+card2.addEventListener("click", card2Visit);
+card3.addEventListener("click", card3Visit);
+card4.addEventListener("click", card4Visit);
+card5.addEventListener("click", card5Visit);
+card6.addEventListener("click", card6Visit);
+
+
+function card1Visit() {
+  window.open("https://en.wikipedia.org/wiki/Plitvice_Lakes_National_Park");
+}
+function card2Visit() {
+  window.open("https://en.wikipedia.org/wiki/Great_Barrier_Reef");
+}
+function card3Visit() {
+  window.open("https://en.wikipedia.org/wiki/Bamboo_Forest_(Kyoto,_Japan)");
+}
+function card4Visit() {
+  window.open("https://en.wikipedia.org/wiki/Plitvice_Lakes_National_Park");
+}
+function card5Visit() {
+  window.open("https://en.wikipedia.org/wiki/Plitvice_Lakes_National_Park");
+}
+function card6Visit() {
+  window.open("https://en.wikipedia.org/wiki/Plitvice_Lakes_National_Park");
+}
+function linkvisit() {
+  window.open("https://en.wikipedia.org/wiki/Nature");
+}
